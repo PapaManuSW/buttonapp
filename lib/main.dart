2f4941e543e4d00412fb66cc8bfe64c07aebc1a0
Massih <massih.z@gmail.com>
2020-05-10 15:50:01 +0200

    Add Google Games signIn and signout
    - Update user model

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,13 @@ class MyApp extends StatelessWidget {
             primaryColor: Color(0xFF01A39D),
             accentColor: Colors.grey[700],
             textTheme: TextTheme(
-                title: TextStyle(color: Colors.white),
-                body2: TextStyle(color: Colors.grey[700]),
-                display1: TextStyle(
+                headline6: TextStyle(color: Colors.white),
+                bodyText2: TextStyle(color: Colors.grey[700]),
+                headline4: TextStyle(
                     color: Colors.grey[700],
                     fontSize: 29.0,
                     fontWeight: FontWeight.w400),
-                display4: TextStyle(
+                headline1: TextStyle(
                     color: Color(0xFF01A39D),
                     fontWeight: FontWeight.w400,
                     fontSize: 90.0),
@@ -46,5 +46,6 @@ class MainPage extends StatelessWidget {
     print('In MainPage');
     print(user);
     return (user == null) ? SignIn() : Home(title: 'The Button App');
+//    return Home(title: 'The Button App');
   }
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,12 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
+  final String uuid;
+  final UserData userData;
+  final GameData gameData;
 
-  final String uid;
-  UserInfo _userInfo;
-  GameData _gameData;
-
-  User({this.uid});
+  User(this.uuid, this.userData, this.gameData);
 }
 
-class UserInfo {}
+class UserData {
+  final String email;
+  final String name;
+  final int
+      notificationInterval; //every how many seconds to send a notification
+  final String registrationToken; //for sending notifications
+  UserData(
+      this.email, this.name, this.notificationInterval, this.registrationToken);
+}
 
-class GameData {}
+class GameData {
+  final int streak;
+  final Timestamp nextClickAt;
+  final int longestStreak;
+
+  GameData(this.streak, this.nextClickAt, this.longestStreak);
+}
